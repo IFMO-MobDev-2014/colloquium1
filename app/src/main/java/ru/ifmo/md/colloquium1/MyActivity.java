8af2c797d8304c43f3192d56d72d8a492467c207
FluffyBear <maksimenkoaleksan96@yandex.ru>
2014-10-07 12:32:37 +0400

    last one 	# Please enter the commit message for your changes. Lines starting 	# with '#' will be ignored, and an empty message aborts the commit. 	# On branch master 	# Your branch is up-to-date with 'origin/master'. 	# 	# Changes to be committed: 	#	modified:   app/src/main/java/ru/ifmo/md/colloquium1/MyActivity.java 	# 	# Changes not staged for commit: 	#	modified:   .idea/gradle.xml 	#	modified:   .idea/misc.xml 	#	modified:   .idea/modules.xml 	#	modified:   app/app.iml 	#	modified:   app/src/main/res/layout/activity_my.xml 	# 	# Untracked files: 	#	.idea/copyright/ 	#	colloquium1.iml 	#

--- app/src/main/java/ru/ifmo/md/colloquium1/MyActivity.java
+++ app/src/main/java/ru/ifmo/md/colloquium1/MyActivity.java
@@ -2,16 +2,23 @@ package ru.ifmo.md.colloquium1;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MyActivity extends Activity {
 
+    private Snake snake;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+        snake = new Snake(this);
+        setContentView(snake);
     }
 
 
@@ -20,6 +27,19 @@ public class MyActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.my, menu);
         return true;
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        snake.resume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        snake.pause();
     }
 
     @Override
