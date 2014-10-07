9d220865adcb2b6b3ea9697f30e733d640c90f21
kirkutirev <kutirev-k@mail.ru>
2014-10-07 12:15:04 +0700

    snake

--- app/src/main/java/ru/ifmo/md/colloquium1/MyActivity.java
+++ app/src/main/java/ru/ifmo/md/colloquium1/MyActivity.java
@@ -1,39 +1,29 @@
-package ru.ifmo.md.colloquium1;
+package ru.ifmo.md.lesson1;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
 public class MyActivity extends Activity {
 
+    private SnakeGame SnakeGame;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my, menu);
-        return true;
+        SnakeGame = new SnakeGame(this);
+        setContentView(SnakeGame);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void onResume() {
+        super.onResume();
+        SnakeGame.resume();
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    @Override
+    public void onPause() {
+        super.onPause();
+        SnakeGame.pause();
     }
 }
