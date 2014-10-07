3b124b0e9ad5d4c4f84726faf78921e2f15c988d
Kuznetsov Nikolay <kna.rus@gmail.com>
2014-10-07 11:47:18 +0400

    Snake game

--- app/src/main/java/ru/ifmo/md/colloquium1/MyActivity.java
+++ app/src/main/java/ru/ifmo/md/colloquium1/MyActivity.java
@@ -4,36 +4,43 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MyActivity extends Activity {
+
+    SnakeView snakeView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my, menu);
-        return true;
+        snakeView = (SnakeView) findViewById(R.id.snakeview);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void onResume() {
+        super.onResume();
+        snakeView.resume();
+    }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+    @Override
+    public void onPause() {
+        super.onPause();
+        snakeView.pause();
+    }
 
-        return super.onOptionsItemSelected(item);
+    public void onButtonLeftClick(View view) {
+        snakeView.turnLeft();
+    }
+
+    public void onButtonRightClick(View view) {
+        snakeView.turnRight();
+    }
+
+    public void onNewGameClick(View view) {
+        snakeView.pause();
+        snakeView.newGame();
+        snakeView.resume();
     }
 }
